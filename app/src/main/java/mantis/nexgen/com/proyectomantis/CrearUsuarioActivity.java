9c98ace83f995a30fde0d99fefd4031ff9b5b67f
NexgenDev <40067459+NexgenDev@users.noreply.github.com>
2018-06-13 10:37:25 -0500

    Implementacon de validaciones basicas de datos e implementacion de metodo "Crear Usuario"

--- app/src/main/java/mantis/nexgen/com/proyectomantis/CrearUsuarioActivity.java
+++ app/src/main/java/mantis/nexgen/com/proyectomantis/CrearUsuarioActivity.java
@@ -1,10 +1,15 @@
 package mantis.nexgen.com.proyectomantis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class CrearUsuarioActivity extends AppCompatActivity {
 
@@ -24,10 +29,70 @@ public class CrearUsuarioActivity extends AppCompatActivity {
         edt_contrasena = findViewById(R.id.edtContrasena);
         edt_repiteContrasena = findViewById(R.id.edtcheckContrasena);
 
+        btn_crearUsuario.setEnabled(false);
+
+        edt_repiteContrasena.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    if(s==edt_contrasena.getText()){
+                        btn_crearUsuario.setEnabled(true);
+                    }else{
+                        btn_crearUsuario.setEnabled(false);
+                    }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        edt_telefono.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().equals("")){
+                    btn_crearUsuario.setEnabled(false);
+                }else{
+                    btn_crearUsuario.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         btn_crearUsuario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                String Nombre = edt_nombre.getText().toString();
+                String Apellidos = edt_apellidos.getText().toString();
+                String Correo = edt_correo.getText().toString();
+                String Contrasena = edt_contrasena.getText().toString();
+                String Telefono = edt_telefono.getText().toString();
+                android.text.format.DateFormat df = new android.text.format.DateFormat();
+                String FechaCreacion = df.format("yyyy-MM-dd hh:mm:ss a", new java.util.Date()).toString();
+                Base_Datos_Interna crearusuario = new Base_Datos_Interna(getApplicationContext());
+                boolean creado = crearusuario.CrearUsuario(Nombre,Apellidos,Contrasena,Telefono,Correo,FechaCreacion);
+                if(creado){
+                    Toast.makeText(getApplicationContext(),"Usuario creado - "+FechaCreacion,Toast.LENGTH_LONG).show();
+                    Intent volverMain = new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(volverMain);
+                }else
+                {
+                    Toast.makeText(getApplicationContext(),"Usuario no pudo ser creado",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }

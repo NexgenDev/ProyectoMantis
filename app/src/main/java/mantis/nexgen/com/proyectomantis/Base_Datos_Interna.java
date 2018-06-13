0cf5f669d54a2aa7fbf23bd2e326f8e292f4ea23
NexgenDev <40067459+NexgenDev@users.noreply.github.com>
2018-06-13 09:58:00 -0500

    Creación clase Base de datos SQLite y creación funcion "Crear Usuario"

---
+++ app/src/main/java/mantis/nexgen/com/proyectomantis/Base_Datos_Interna.java
@@ -0,0 +1,41 @@
+package mantis.nexgen.com.proyectomantis;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Base_Datos_Interna extends SQLiteOpenHelper {
+    static final String DB_NOMBRE = "dbinterna";
+    static final int DB_VERSION = 1;
+    static final String TABLA_USUARIO = "CREATE TABLE USUARIOS (NOMBRE TEXT,APELLIDOS TEXT,CORREO TEXT,TELEFONO TEXT UNIQUE,CONTRASENA TEXT,FECHACREACION TEXT)";
+    public Base_Datos_Interna(Context context) {
+        super(context, DB_NOMBRE, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        if(db !=null) {
+            db.execSQL(TABLA_USUARIO);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE USUARIOS");
+        db.execSQL(TABLA_USUARIO);
+    }
+    public boolean CrearUsuario (String Nombre,String Apellidos,String Contrasena,String Telefono,String Correo,String FechaCreacion){
+        boolean usuario_creado=false;
+        SQLiteDatabase db = getWritableDatabase();
+        if (db!=null){
+            try{
+                db.execSQL("INSERT INTO USUARIOS VALUES ("+Nombre+","+Apellidos+","+Correo+","+Telefono+","+FechaCreacion+");");
+                usuario_creado=true;
+            }catch(SQLException ex){
+                usuario_creado=false;
+            }
+        }
+        return usuario_creado;
+    }
+}

151b64989d6a9eafc165484976c038edbb6a2466
NexgenDev <40067459+NexgenDev@users.noreply.github.com>
2018-06-19 15:32:17 -0500

    Formulario fitosanitario funcionando hasta la consulta de ubicación

--- app/src/main/java/mantis/nexgen/com/proyectomantis/ConsultasExternas.java
+++ app/src/main/java/mantis/nexgen/com/proyectomantis/ConsultasExternas.java
@@ -1,9 +1,8 @@
 package mantis.nexgen.com.proyectomantis;
 
+
 import android.content.Context;
 import android.content.Intent;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -15,7 +14,7 @@ import com.android.volley.toolbox.Volley;
 
 import java.util.HashMap;
 
-public class ConsultasExternas {
+public class ConsultasExternas{
     Context context;
     public ConsultasExternas(Context context) {
         this.context = context;
@@ -48,33 +47,6 @@ public class ConsultasExternas {
             protected HashMap<String,String> getParams(){
                 HashMap<String,String> params = new HashMap<>();
                 params.put("usuario",usuario);
-                return params;
-            }
-        };
-        RequestQueue requestQueue = Volley.newRequestQueue(context);
-        requestQueue.add(stringRequest);
-    }
-    public void Get_Producto (final Context context, final String usuario){
-        String URL = "http://stage.inteli-bpm.com/Portal/ws/soap/cliente_mantis.php";
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                String[] respuesta =  response.split("&");
-                String[] producto = respuesta[0].split(",");
-                ArrayAdapter adpProducto = new ArrayAdapter(context,android.R.layout.simple_spinner_dropdown_item,producto);
-                adpProducto.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(context,"Error: "+error,Toast.LENGTH_SHORT).show();
-            }
-        }
-        ){
-            protected HashMap<String,String> getParams(){
-                HashMap<String,String> params = new HashMap<>();
-                params.put("id_usuario",usuario);
                 return params;
             }
         };
